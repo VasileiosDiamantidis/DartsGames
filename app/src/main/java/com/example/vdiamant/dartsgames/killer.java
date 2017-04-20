76524a29dbfafdc3f40e7ea68802a52140a73fb6
vdiamantGR <basdiamantidis@gmail.com>
2017-04-20 12:49:50 +0300

    Create Fragment for the point system implemented to killerGame

--- app/src/main/java/com/example/vdiamant/dartsgames/killer.java
+++ app/src/main/java/com/example/vdiamant/dartsgames/killer.java
@@ -15,20 +15,22 @@ import org.xmlpull.v1.XmlPullParserException;
 import java.io.IOException;
 import java.util.List;
 
-public class killer extends AppCompatActivity {
-
+public class killer extends AppCompatActivity implements ucAddPoints.ucAddPointsListener{
+/*
     List<Player> players;
     Player whoIsPlaying;
     int maxDarts=3;
     int playingDartsLeft=3;
     int numberOfPlayer=0;
     int gamePoints;
-    String game;
+    String game;*/
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_killer);
 
+
+        /*
         Intent myIntent = getIntent();
         players = (List<Player>) myIntent.getSerializableExtra("LIST");
         game=myIntent.getStringExtra(DartsGames.EXTRA_MESSAGE);
@@ -37,7 +39,17 @@ public class killer extends AppCompatActivity {
         //setOterPlayers();
         //initialize();
         //setFirstInPointsTop();
+        */
 
     }
 
+    @Override
+    public void addPoints(int points) {
+        //this is called when the user clicks the ok Button
+
+        //An eixame 2o fragment
+        //OnomaDeuterouFragment btmFragment=(OnomaDeuterouFragment)getSupportFragmentManager().findFragmentById(R.id.fragment2); //to fragment2 einai apo thn xml toy killer
+        //btmFragment.methodos();
+
+    }
 }
